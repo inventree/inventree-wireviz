4e1c66a03999456572519dd4455748f23f04ad2f
Oliver Walters <oliver.henry.walters@gmail.com>
2025-04-04 04:45:15 +0000

    Update packaging requirements

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteExternalsPlugin } from 'vite-plugin-externals'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: 'classic'
+    }),
+    viteExternalsPlugin({
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      '@mantine/core': 'MantineCore',
+      "@mantine/notifications": 'MantineNotifications',
+    }),
+  ],
+  esbuild: {
+    jsx: 'preserve',
+  },
   build: {
     cssCodeSplit: false,
     manifest: false,
@@ -17,7 +31,17 @@ export default defineConfig({
         dir: '../inventree_wireviz/static',
         entryFileNames: '[name].js',
         assetFileNames: 'assets/[name].[ext]',
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          '@mantine/core': 'MantineCore',
+          "@mantine/notifications": 'MantineNotifications',
+        },
       },
-      }
-  }
+      external: ['react', 'react-dom', '@mantine/core', '@mantine/notifications'],
+    }
+  },
+  optimizeDeps: {
+    exclude: ['react', 'react-dom', '@mantine/core', '@mantine/notifications'],
+  },
 })
